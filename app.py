881828a8fc2421a0d3451c9d7685d3e052ae2028
Edresson Casanova <edresson1@gmail.com>
2020-04-01 08:15:11 -0300

    update

--- app.py
+++ app.py
@@ -19,6 +19,7 @@ from config import config
 
 # csv data from make dataset
 data_csv = 'static/Dataset/metadata.csv'
+data_validated_csv = 'static/Dataset/metadata_validated.csv'
 
 app = Flask(__name__)
 app.config.from_object(config['dev'])
@@ -54,6 +55,24 @@ def initdataset():
         new_data.file_path= audio_path
         new_data.file_with_user = 0 # 1 if user validating this instance
         new_data.instance_validated = 0 #1 if human validated this instance
+        new_data.instance_valid = 0 # 1 if instance is ok
+        db.session.add(new_data)
+    db.session.commit()
+
+
+@manager.command
+def initvalidateddataset():
+    lines = list(open(data_validated_csv).readlines())
+
+    for line in lines:
+        audio_path,lenght,text = line.split(',')
+        text = text.replace('\n','')
+        new_data= Dataset()
+        new_data.text = text
+        new_data.audio_lenght = lenght
+        new_data.file_path= audio_path
+        new_data.file_with_user = 0 # 1 if user validating this instance
+        new_data.instance_validated = 1 #1 if human validated this instance
         new_data.instance_valid = 0 # 1 if instance is ok
         db.session.add(new_data)
     db.session.commit()
